b97fb8ae1297fdcad0f92f6a1a8bc58a5fc7f91f
Mateus Loeser Reisdorfer <79878580+NthMateus28@users.noreply.github.com>
2023-10-03 08:35:06 -0300

    Update menu.js

--- FinalProject/menu.js
+++ FinalProject/menu.js
@@ -1,6 +1,13 @@
 const menuIcon = document.getElementById('menu-icon');
 const mobileMenu = document.getElementById('mobile-menu');
+const menuImage = document.getElementById('menu-image');
 
 menuIcon.addEventListener('click', () => {
-    mobileMenu.classList.toggle('open'); // Adicione ou remova a classe 'open'
-});
+    mobileMenu.classList.toggle('open');
+
+        if (menuImage.src.includes("menu.png")) {
+            menuImage.src = "../img/x.png"; 
+        } else {
+            menuImage.src = "../img/menu.png"; 
+        }
+    });

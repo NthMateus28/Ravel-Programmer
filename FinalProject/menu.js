ea8c22016b298e91d7882272eeca4a4b1f2c0a06
Mateus Loeser Reisdorfer <79878580+NthMateus28@users.noreply.github.com>
2023-10-05 21:18:01 -0300

    Update menu.js

--- FinalProject/menu.js
+++ FinalProject/menu.js
@@ -5,6 +5,7 @@ const menuImage = document.getElementById('menu-image');
 menuIcon.addEventListener('click', () => {
     mobileMenu.classList.toggle('open');
 
+    //O método includes() é uma maneira simples e eficiente de verificar se uma string contém um determinado valor em JavaScript.
         if (menuImage.src.includes("menu.png")) {
             menuImage.src = "../img/x.png"; 
         } else {
